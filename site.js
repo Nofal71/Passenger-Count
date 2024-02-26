566f28d8ebe18987b0b18b3bef1ae49d0f60def0
Nofal71 <hafiznofal76@gmail.com>
2024-02-26 17:30:09 +0500

    Alert

--- site.js
+++ site.js
@@ -56,3 +56,10 @@ function back(){
     document.getElementById("data").style.display = "none";
     document.getElementById("main").style.display = "block";
 }
+
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    e.returnValue = '';
+    var confirmationMessage = 'Are you sure you want to leave this page?';
+    return confirmationMessage;
+});
